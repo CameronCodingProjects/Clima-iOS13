2fc1c91b79434a4d6c13422312b47e4a697e1a1c
CameronCodingProjects <cameron2733@gmail.com>
2022-02-04 12:51:52 -0500

    Extensions

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -16,16 +16,55 @@ class WeatherViewController: UIViewController {
     
     @IBOutlet weak var searchTextField: UITextField!
     
-    // UI button search bar when pressed
-    @IBAction func searchPressed(_ sender: UIButton) {
-        print(searchTextField.text!)
-    }
-    
+    var weatherManager = WeatherManager()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        weatherManager.delegate = self
+        searchTextField.delegate = self
     }
-
-
 }
 
+//MARK: - UITextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate {
+    // UI button search bar when pressed
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
+}
+
+//MARK: - WeatherManagerDelegate
+extension WeatherViewController: WeatherManagerDelegate{
+    func didUpdateWeather(_ weatherManager: WeatherManager , weather: WeatherModel)
+    {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conidtionName)
+        }
+    }
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
